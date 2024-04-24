617466584f9678ab9fa7098d0571b325c2da8527
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-04-24 15:11:45 -0500

    decorator pttrn using highorderfunc

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dev-crusader404/go-test-project
 
 go 1.21.8
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
-	val "github.com/dev-crusader404/go-test-project/codingchallenge"
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
 )
 
+type DB interface {
+	Store(s string) error
+}
+
+type Store struct{}
+
+func (s *Store) Store(a string) error {
+	fmt.Printf("\nStoring the value: %s", a)
+	return nil
+}
+
+func makeHTTPFunc(db DB, hd myHandlerFunc) http.HandlerFunc {
+	fmt.Println("creating the makeHTTPFunc")
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := hd(db, w, r); err != nil {
+			http.Error(w, err.Error(), 500)
+		}
+		db.Store("Key to success")
+	}
+}
+
+func handler(db DB, w http.ResponseWriter, r *http.Request) error {
+	fmt.Println("I am at handler function")
+	db.Store("Way through the key")
+	return nil
+}
+
+type myHandlerFunc func(db DB, w http.ResponseWriter, r *http.Request) error
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestID := r.Header.Get("X-Request-ID")
+		if requestID == "" {
+			requestID = uuid.New().String()
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "RequestID", requestID)
+		r = r.WithContext(ctx)
+		next(w, r)
+	}
+}
+
 func main() {
-	val.RunCreditCardValidator()
+	// val.RunCreditCardValidator()
+	s := &Store{}
+	http.HandleFunc("/", Logger(makeHTTPFunc(s, handler)))
+	http.ListenAndServe(":8080", nil)
 }
