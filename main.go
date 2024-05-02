bc9471057178ee527be36d60a7c299923969ef81
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-02 17:20:10 -0500

    code refactored

--- main.go
+++ main.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/google/uuid"
+	sv "github.com/dev-crusader404/go-test-project/restapi"
 )
 
-type DB interface {
-	Store(s string) error
-}
-
-type Store struct{}
-
-func (s *Store) Store(a string) error {
-	fmt.Printf("\nStoring the value: %s", a)
-	return nil
-}
-
-func makeHTTPFunc(db DB, hd myHandlerFunc) http.HandlerFunc {
-	fmt.Println("creating the makeHTTPFunc")
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := hd(db, w, r); err != nil {
-			http.Error(w, err.Error(), 500)
-		}
-		db.Store("Key to success")
-	}
-}
-
-func handler(db DB, w http.ResponseWriter, r *http.Request) error {
-	ctx := r.Context()
-	reqID := ctx.Value("RequestID").(string)
-
-	fmt.Println("I am at handler function")
-	db.Store("Way through the key: " + reqID)
-	time.Sleep(10 * time.Second)
-	resp, _ := json.Marshal(map[string]any{"message": fmt.Sprintf("Successfully processed requestID: %s", reqID)})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
-	return nil
-}
-
-type myHandlerFunc func(db DB, w http.ResponseWriter, r *http.Request) error
-
-func Logger(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		requestID := r.Header.Get("X-Request-ID")
-		if requestID == "" {
-			requestID = uuid.New().String()
-		}
-		ctx := r.Context()
-		ctx = context.WithValue(ctx, "RequestID", requestID)
-		r = r.WithContext(ctx)
-		next(w, r)
-	}
-}
+var (
+	logger       = sv.Logger
+	makeHTTPFunc = sv.MakeHTTPFunc
+)
 
 func main() {
 	// val.RunCreditCardValidator()
-	s := &Store{}
-	http.HandleFunc("/", Logger(makeHTTPFunc(s, handler)))
+	s := sv.NewDB()
+	http.HandleFunc("/", logger(makeHTTPFunc(s, sv.Handler)))
 	http.ListenAndServe(":8080", nil)
 }
---
+++ restapi/api.go
@@ -0,0 +1,84 @@
+package restapi
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type DB interface {
+	Store(s string) error
+}
+
+type Store struct{}
+
+func NewDB() DB {
+	return &Store{}
+}
+
+func (s *Store) Store(a string) error {
+	fmt.Printf("\nStoring the value: %s", a)
+	return nil
+}
+
+func MakeHTTPFunc(db DB, hd myHandlerFunc) http.HandlerFunc {
+	fmt.Println("creating the makeHTTPFunc")
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := hd(db, w, r); err != nil {
+			http.Error(w, err.Error(), 500)
+		}
+		db.Store("Key to success")
+	}
+}
+
+func Handler(db DB, w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+	reqID := ctx.Value("RequestID").(string)
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	defer r.Body.Close()
+
+	request := struct {
+		Message string `json:"body"`
+	}{}
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	fmt.Println("I am at handler function")
+	db.Store("Way through the key: " + reqID)
+	time.Sleep(10 * time.Second)
+	resp, _ := json.Marshal(map[string]any{"status": fmt.Sprintf("Successfully processed requestID: %s", reqID),
+		"message": request.Message})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+	return nil
+}
+
+type myHandlerFunc func(db DB, w http.ResponseWriter, r *http.Request) error
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("logger init")
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestID := r.Header.Get("X-Request-ID")
+		if requestID == "" {
+			requestID = uuid.New().String()
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "RequestID", requestID)
+		r = r.WithContext(ctx)
+		next(w, r)
+	}
+}
